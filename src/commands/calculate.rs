eb97cd05da4d4dfa010803da9822a36c0b72b17e
Sam Davidoff <sam@mfbtech.com>
2025-12-12 16:08:35 +0000

    add calculate hash function

---
+++ src/commands/calculate.rs
@@ -0,0 +1,30 @@
+use std::io;
+use std::path::Path;
+
+use crate::utils::hash::hash_file;
+
+pub fn run(file: &Path) -> io::Result<String> {
+    hash_file(file)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Write;
+    use tempfile::NamedTempFile;
+
+    fn create_temp_file(content: &[u8]) -> NamedTempFile {
+        let mut file: NamedTempFile = NamedTempFile::new().unwrap();
+        file.write_all(content).unwrap();
+        file
+    }
+
+    #[test]
+    fn returns_expected_hash() {
+        let file = create_temp_file(b"test content");
+        let hash = run(file.path()).unwrap();
+
+        assert_eq!(hash.as_str(), "6ae8a75555209fd6c44157c0aed8016e763ff435a19cf186f76863140143ff72");
+    }
+}
+
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,2 +1,3 @@
+pub mod calculate;
 pub mod compare;
 pub mod check;
